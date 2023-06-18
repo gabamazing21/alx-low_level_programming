92a561fff58dba4755e3e8a752fc0eb8143c9e3b
root <gabamazing21@gmail.com>
2023-06-18 05:09:57 -0700

    improve on the betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/**
+ *  main - print string
+ *  return: 0
+ */
 int main(void)
 {
 	char programming[] = "Programming is like building a multilingual puzzle";
 
 	puts(programming);
-	return (0);
+	ireturn (0);
 }
