085d953e48e927ebfa0933dd9a2405d53413b275
root <gabamazing21@gmail.com>
2023-06-18 05:28:56 -0700

    error solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char programming[] = "Programming is like building a multilingual puzzle";
+	char programming[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(programming);
 	return (0);
