619454436a1458a3a9c9619a10ea30ce6aeebe0e
root <gabamazing21@gmail.com>
2023-06-19 00:10:26 -0700

    corrected 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Description: getting size of the data type in c,
  * int, long int, long long int, char, float
  *
- * Return : Always return 0 (success)
+ * Return: Always return 0 (success)
  */
 int main(void)
 {
@@ -14,11 +14,11 @@ int main(void)
 	float f;
 	long int li;
 	long long int lli;
-	
-	printf(" Size of an int is : %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf(" Size of a long int is : %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf(" Size of a char is : %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf(" Size of a float is : %lu byte(s)\n", (unsigned long)sizeof(f));
-	printf(" Size of a long long int is : %lu byte(s)\n", (unsigned long)sizeof(lli));
+
+	printf(" Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf(" Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf(" Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf(" Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf(" Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	return (0);
 }
