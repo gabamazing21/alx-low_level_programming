223f45f4f6e4a592badf94ddd96dfcf96d1877b0
root <gabamazing21@gmail.com>
2023-06-19 20:07:30 -0700

    added 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing all the digit decimal number
+ * without using printf
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10; n++)
+		putchar(n + '0');
+	putchar('\n');
+	return (0);
+}
