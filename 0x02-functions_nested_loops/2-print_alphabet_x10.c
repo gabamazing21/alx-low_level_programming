22ea6a8532eb910491b510ad3a8a2f4901f08717
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-21 04:17:55 +0100

    fixed betty issue

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - printing small alphabet letter
+ * main - printing small alphabet letter a-z 10 time
  * using _putchar
  * Return: always 0
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void);
 {
 	char a;
 
+	
 	for (a = 'a' ; a <= 'z' ; a++)
 	{
 		_putchar(a);
