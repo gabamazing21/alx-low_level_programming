214d663ce820b08ae549aaaef17f6a10407624e2
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-21 03:10:43 +0100

    i added the three file main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,12 @@
-#include <unistd.h>
-
+int _putchar(char c);
+void print_alphabet(void);
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check the code
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+    print_alphabet();
+    return (0);
 }
