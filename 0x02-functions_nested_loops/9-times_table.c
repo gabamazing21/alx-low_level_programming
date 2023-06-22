2e08635c3cd62ffece683959bf781792b78d5781
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-22 17:34:33 +0100

    added new directory 0x04-more_functions_nested_loops with 0-isuper to check upper case letter:

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int i, j, multiples;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			multiples = i * j;
+
+			if (multiples >= 10)
+			{
+				_putchar('0' + multiples / 10);
+				_putchar('0' + multiples % 10);
+			}
+			else
+			{
+				if (j != 0) /* Don't print leading space for first column */
+				{
+					_putchar(' '); /* Print a space before single digit numbers, except for the first column */
+				}
+				_putchar('0' + multiples);
+			}
+			if (j != 9) /* Don't print comma and space after the last column */
+			{
+				_putchar(',');
+				_putchar(' ');
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+learning more on functions loops
