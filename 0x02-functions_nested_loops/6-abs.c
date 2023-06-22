f9252ef78d531c2ccde02093fc6765baec771b09
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-22 02:47:23 +0100

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - return int absolute value
+ * @n: charecter to print abs
+ * Return: It return abs n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+	else
+	{
+		return (n);
+	}
+}
