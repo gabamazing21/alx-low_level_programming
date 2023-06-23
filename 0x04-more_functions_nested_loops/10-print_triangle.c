cf704e18347043b3fbec152f2b1bfd4886902b90
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-23 02:26:52 +0100

    added how to draw triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,3 +1,26 @@
-#include <stdio.h>
+#include "main.h"
+
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if(size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (row = 0; row < size; row++)
+	{
+		for (spaces = 0; spaces <= size -row; spaces++)
+		{
+			_putchar(' ');
+		}
+		for (hashes = 0; hashes <= row; hashes++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
 
 
