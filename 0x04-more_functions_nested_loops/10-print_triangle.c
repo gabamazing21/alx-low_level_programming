5814861e7b6f1105b8fef92677f4d737d65aa55f
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-23 02:34:12 +0100

    print triangle function added

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,17 +1,20 @@
 #include "main.h"
-
+/**
+ * print_triangle - draw triangle
+ * @size: size of triangle
+ */
 void print_triangle(int size)
 {
 	int row, hashes, spaces;
 
-	if(size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 	for (row = 0; row < size; row++)
 	{
-		for (spaces = 0; spaces <= size -row; spaces++)
+		for (spaces = 0; spaces <= size - row; spaces++)
 		{
 			_putchar(' ');
 		}
