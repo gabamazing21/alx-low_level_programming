6a05802199d8b0e8093ad91611fda3469759a0d9
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-26 22:56:27 +0100

    print string in reverse order file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_rev - to write string in reverse
+ * @s: character to print
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length = _strlen(s);
+
+	for (i = length ; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+/**
+  * _strlen - get length of string
+  * @s: character to check
+  * Return: it return length
+  */
+int _strlen(char *s)
+{
+	int length = 0;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+
+	}
+	return (length);
+}
