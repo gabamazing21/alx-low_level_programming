e974a197313b4c1d846b2020fded4eabc3bf125c
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-27 02:51:38 +0100

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print element of array of integer
+ * @a: input parameter
+ * @n: input parameter
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d ,", a[i]);
+	}
+	printf("\n");
+}
