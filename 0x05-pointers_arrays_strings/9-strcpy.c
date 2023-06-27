9f26ba57b18fae6cd50c57ed50ba1e0aa56d5353
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-27 03:56:33 +0100

    copy from src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcpy - copy array from src to dest
+ * @dest: input parameter destination
+ * @src: input parameter src
+ * Return: it return dest array;
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i + 1] = '\0';
+
+	return (dest);
+}
