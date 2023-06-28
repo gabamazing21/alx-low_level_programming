27f552e7b43c7236c40bf36a7be1d2b408f39241
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-28 04:58:47 +0100

    advance task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
  * _atoi - it convert string to integer
  * @s: input parameter
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+
+void crackme(int N)
+{
+	int i = 0;
+	int randomizer = 0;
+
+}
