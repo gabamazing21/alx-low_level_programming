9ec20bba324d6b21a3c902c112a1b4d8c5d91dce
Gabriel Daramola <gabamazing21@gmail.com>
2023-06-28 04:33:46 +0100

    added debbuggin

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * _atoi - it convert string to integer
  * @s: input parameter
@@ -9,24 +11,19 @@ int _atoi(char *s)
 	int i = 0;
 	int num = 0;
 	int is_negative = 1;
-	int digit_found = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == '-' || s[i] == '+')
-		{
-			if (s[i] == '-')
+		if (s[i] == '-')
 				is_negative *= -1;
-		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			num = num * 10 + (s[i] - '0');
-			digit_found = 1;
+			num *= 10;
+			num += is_negative*(s[i] - '0');
 		}
-		else if (digit_found)
+		else if (num !=0)
 			break;
-		i++;
 	}
-	return (num * is_negative);
+	return (num);
 }
 
