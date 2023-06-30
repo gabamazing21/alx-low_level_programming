fc47308cb6ad5a49b4e80b973f15c4068509f8fb
Gabriel Daramola <gabamazing21@gmail.com>
2023-07-01 00:05:27 +0100

    functions that encode string in rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,4 @@
 #include "main.h"
-int _isalpha(int c);
-int _islower(int c);
 /**
   * rot13 - encode a string using rot13
   * @a: character input
@@ -8,56 +6,18 @@ int _islower(int c);
   */
 char *rot13(char *a)
 {
-	int i;
+	int i, j;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (_isalpha(a[i]) == 1)
+		for (j = 0; input[j] != '\0'; j++)
 		{
-			if (_islower(a[i]) == 1)
-			{
-				a[i] = (((a[i] - 'a') + 13) % 26) + 'a';
-			}
-			else
-			{
-				a[i] = (((a[i] - 'A') + 13) % 26) + 'A';
-			}
+			if (a[i] == input[j])
+				a[i] = output[j];
 		}
 	}
 	return (a);
 }
 
-/**
- * _isalpha- check if char isalpha
- * @c: character to check isalpha
- * Return: always result
- */
-int _isalpha(int c)
-{
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-		return (1);
-	else
-		return (0);
-}
-
-#include "main.h"
-/**
- * _islower - check if char islower
- * @c: character to check islower
- * Return: always result
- */
-int _islower(int c)
-{
-	char a;
-	int result = 0;
-
-	for (a = 'a' ; a <= 'z' ; a++)
-	{
-		if (c == a)
-		{
-			result = 1;
-			break;
-		}
-	}
-	return (result);
-}
