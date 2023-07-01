a55dd98e123b59fcb23719fab33386c29f59e906
Gabriel Daramola <gabamazing21@gmail.com>
2023-07-01 05:41:53 +0100

    done

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+/**
+  * main- solve problem
+  * Return: nothing
+  */
+#include <stdio.h>
+
+int main(void)
+{
+	int a[5] = {98, 198, 298, 398, 498};
+	int *p;
+
+	k = a;
+	k += 2;
+	printf("a[2] = %d\n", *k);
+
+	return (0);
+}
