58b6bc9eaad7fa98af3817ce9dc3730f345240c7
Gabriel Daramola <gabamazing21@gmail.com>
2023-07-04 23:27:53 +0100

    print sum of 2 diagonal in a 2d array

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,15 +7,15 @@
   */
 void print_diagsums(int *a, int size)
 {
-	int i, j, suma = 0, sumb = 0;
+	int i, suma = 0, sumb = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		suma += a[(size + 1) * i];
 	}
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
-		sumb += a[(size - 1) * (j + 1)];
-	} 
-	printf("%d, %d\n ", suma, sumb);
+		sumb += a[(size - 1) * (i + 1)];
+	}
+	printf("%d, %d\n", suma, sumb);
 }
