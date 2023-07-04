ccb80320edd2298cc1b39b7e5ddc2158d99d9902
Gabriel Daramola <gabamazing21@gmail.com>
2023-07-04 03:47:44 +0100

    function that locates a substring.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,12 +9,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		if (haystack[i] == needle[0])
 		{
-			if (needle[i] == haystack[j])
-				return (&haystack[j]);
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
 		}
 	}
 	return (0);
