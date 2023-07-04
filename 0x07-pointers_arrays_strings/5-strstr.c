2c52d5daf516017838ca93f8b8ebad7dfce526da
Gabriel Daramola <gabamazing21@gmail.com>
2023-07-04 04:47:10 +0100

    debugging

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,16 +12,14 @@ char *_strstr(char *haystack, char *needle)
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-					break;
-			}
-			if (needle[j] == '\0')
-			{
-				return (&haystack[i]);
-			}
-			if(haystack[i + j] == '\0')
-				return (0);
+		{
+			if (haystack[i + j] != needle[j])
+			break;
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
 	}
 	return (0);
 }
