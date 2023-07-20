38d638486dc665f87ac02c27527c972c066161d5
Gabriel Daramola <gabamazing21@gmail.com>
2023-07-20 23:42:45 +0100

    sum them all function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+/**
+  * sum_them_all - sum of numbers;
+  * @n: count
+  * @...: variable of number of argument
+  * Return: return sum
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int sum;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+working with variadic and const function
