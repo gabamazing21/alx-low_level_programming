89ee9c71777a06b29686356c8f5315df411e5c4d
Gabriel Daramola <gabamazing21@gmail.com>
2023-07-20 05:46:22 +0100

    k

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,11 @@
 #include "function_pointers.h"
+/**
+  * array_iterator - iterate
+  * @array: aarra
+  * @size: size
+  * @action: function
+  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
