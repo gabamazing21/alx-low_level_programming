7e39d95cca4e6581c095a0b50df72daf422405bd
Gabriel Daramola <gabamazing21@gmail.com>
2023-07-31 17:15:45 +0100

    print linkedlist

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+  * print_list - print element in a linkedinlist
+  * @h: list to be inserted
+  * Return: number of nodes
+  */
+
+size_t print_list(const list_t *h)
+{
+	int n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+			return (0);
+		}
+		else
+		{
+			printf("[%d]", h->len);
+			printf(" %s \n", h->str);
+			h = h->next;
+			n++;
+		}
+	}
+	return (n);
+}
