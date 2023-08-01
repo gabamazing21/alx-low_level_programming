a367eaf36c1ad7cd029be9f1d76eb8615f5c3626
Gabriel Daramola <gabamazing21@gmail.com>
2023-08-01 23:50:44 +0100

    function to print all element in listint linkedlist

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  * print_listint - prints all elements in listint_t
+  * @h: header node
+  * Return: return number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
