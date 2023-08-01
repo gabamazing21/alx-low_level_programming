62baac9acf6df1a8a825410d7ccc5bfb6bc9ab04
Gabriel Daramola <gabamazing21@gmail.com>
2023-08-02 00:57:37 +0100

    function to free listint_t list which has double pointer has arguement

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+  * free_listint2 - free listint_t list
+  * @header: pointer to pointer header node
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
