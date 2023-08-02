ea6a3fcfd73b70ba15da5102e941f55f44c9bfff
Gabriel Daramola <gabamazing21@gmail.com>
2023-08-02 10:34:09 +0100

    delete node at insert index positon

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,15 +10,27 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current, *temp;
 
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
 	current = *head;
 	while (current != NULL && i < index - 1)
 	{
 		current = current->next;
-		temp = current;
 		i++;
 	}
-	if (current == NULL)
+
+	if (current == NULL || current->next == NULL)
 		return (-1);
-	temp->next = current->next;
+	temp = current->next;
+	current->next = temp->next;
+	free(temp);
 	return (1);
 }
