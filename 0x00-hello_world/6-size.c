7659570c7d3c8ebd2b8803510735d9d76e785b71
root <gabamazing21@gmail.com>
2023-08-02 14:36:24 -0700

    print varios data type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,17 +9,10 @@
  */
 int main(void)
 {
-	int i;
-	char c;
-	float f;
-	long int j;
-	long long int k;
-
-
-	printf(" Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf(" Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(j));
-	printf(" Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf(" Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	printf(" Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf(" Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf(" Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf(" Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf(" Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf(" Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
