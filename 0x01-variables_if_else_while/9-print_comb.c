746a2fb8762f85c293dc49a3a1b03c0bb8b6f583
Gabriel Daramola <gabamazing21@gmail.com>
2023-08-03 00:01:19 +0100

    print single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - print possible combination for single digit
+  * Return: return 0
+  */
+int main(void)
+{
+	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		putchar (num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
