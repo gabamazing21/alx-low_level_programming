bfb08ea4ebef6153fd001b064a654882decfe4f0
Gabriel Daramola <gabamazing21@gmail.com>
2023-08-02 01:03:34 +0100

    free int linked list with double pointer as header

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
   * free_listint2 - free listint_t list
-  * @header: pointer to pointer header node
+  * @head: pointer to pointer header node
   */
 void free_listint2(listint_t **head)
 {
@@ -13,4 +13,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
+	*head = NULL;
 }
