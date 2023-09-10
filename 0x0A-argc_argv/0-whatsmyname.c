2b0ad3acc30ff342b3cad0390d186f66a65c8b40
gabriel <gabeamazing21@gmail.com>
2023-09-10 07:45:03 +0000

    print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - write file name
+ * @argc: main argument count
+ * @argv: main argument value
+ * Return: always zero
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
