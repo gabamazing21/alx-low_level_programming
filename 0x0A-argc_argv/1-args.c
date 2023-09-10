07b773ff628a36a8fb909cbc468ed2a9c128f715
gabriel <gabeamazing21@gmail.com>
2023-09-10 07:53:11 +0000

    print argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print argument count
+ * @argc: argument count
+ * @argv: argument value
+ * Return: always zero
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
