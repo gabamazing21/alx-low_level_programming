856602edd8eb75dedac78161dc064b86e0ce7f25
gabriel <gabeamazing21@gmail.com>
2023-09-12 20:54:46 +0000

    add docs to dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,10 @@
+#include "dog.h"
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *dog;
+	d= &dog;
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
 
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
+/**
+ * struct dog - object for dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog oject which has name, age, owner
+ */
 struct dog
 {
 	char *name;
