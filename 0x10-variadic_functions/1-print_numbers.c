f2efe7a0ca0fed656d5473d11bc3b2287bd317fd
gabriel <gabeamazing21@gmail.com>
2023-09-15 04:34:46 +0000

    print numbers in c using var

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - it prints number followed by new lines
+ * @separator: separtor to used
+ * @n:number of integers passed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
 
+	va_start(ptr, n);
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d%s ", va_arg(ptr, unsigned int), separator);
+		}
+		printf("\n");
+	}
+
+	va_end(ptr);
+}
