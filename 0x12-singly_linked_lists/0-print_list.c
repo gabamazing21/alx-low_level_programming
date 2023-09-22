0d2513e755f5affdcdf9b366a7fb8bf186983f4b
gabriel <gabeamazing21@gmail.com>
2023-09-22 11:55:38 +0000

    print node list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_list - print element of list_t
+ * @h: pointer to node
+ * Return: return number of results
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
