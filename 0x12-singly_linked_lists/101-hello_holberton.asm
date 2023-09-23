af81bbb645a31b35961ffe7b1bd54376d009da23
gabriel <gabeamazing21@gmail.com>
2023-09-23 14:49:31 +0000

    print hoberlton in c using c llibray

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,10 @@
-section .rodata
-	msg: db "Hello, Holberton",10
-	msglen: equ $ - msg
+	global main
+	extern puts
 
-section .text
-
-_start:
-	mov rax, 1		;write(
-	mov rdi, 1		;	STDOUT_FILENO,
-	mov rsi, msg		;	"Hello, Holberton\n",
-	mov rdx, msglen		;	sizeof("Hello, Holberton\n")
-	syscall			;);
-
-	mov rax, 60		;exit(
-	mov rdi, 0		; EXIT_SUCCESS
-	syscall			; )  
+	section .text
+main:
+	mov	rdi, message
+	call	puts
+	ret
+message:
+	db	"Hello, Holberton", 0
