732da1924759ef2a03313c997cf6cc7bf4048fb8
gabriel <gabeamazing21@gmail.com>
2023-09-24 20:22:51 +0000

    print int using putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_number - print int using char
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+		print_long(n);
+	else
+	{
+		_putchar('0' + n);
+	}
+}
+/**
+ * print_long - print int with putchar
+ * @value: int
+ */
+void print_long(int value)
+{
+	if (value != 0)
+	{
+		print_long(value / 10);
+		_putchar((value % 10) + '0');
+	}
+}
