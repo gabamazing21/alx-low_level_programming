ec69d10ccfb8c0298172d04929ef27dc3f2cfea0
gabriel <gabeamazing21@gmail.com>
2023-09-26 07:20:01 +0000

    crack file password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,64 +1,27 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <time.h>
 /**
-  * crackme - it crack word
-  * @N: input int
-  */
-void crackme(int N)
-{
-	int i = 0;
-	int randomizer = 0;
-
-	srand((unsigned int) (time(NULL)));
-	char numbers[] = "01234567890";
-	char letter[] = "abcdefghijklmnopqrstuvwxyz";
-	char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char symbols[] = "!@#$^&*?";
-	char password[N];
-
-	randomizer = rand() % 4;
-	for (i = 0; i < N; i++)
-	{
-		if (randomizer == 1)
-		{
-			password[i] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 2)
-		{
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3)
-		{
-			password[i] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-
-		}
-		else
-		{
-			password[i] = letter[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-
-		}
-	}
-}
-/**
-  * main - it run the code
-  * Return: always return zero
-  */
+ * main - generate random password to crack file
+ * Return: always (0) success
+ */
 int main(void)
 {
-	int u;
+	int pass[64], i, add = 0, n;
 
-	u = 10;
-	crackme(u);
-	putchar('\n');
+	srand(time(NULL));
+	for (i = 0; i < 64; i++)
+	{
+		pass[i] = rand() % 78;
+		add = add + pass[i] + '0';
+		putchar(pass[i] + '0');
+		if ((2772 - add) - 48 < 78)
+		{
+			n = 2772 - add - '0';
+			add = add + n;
+			putchar(n + '0');
+			break;
+		}
+	}
 	return (0);
 }
