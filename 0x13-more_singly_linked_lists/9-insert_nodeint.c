d17964ea8e05e7ab25c662ea4a39eb1ca9771981
gabriel <gabeamazing21@gmail.com>
2023-09-26 10:11:08 +0000

    add node at a position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,20 +8,55 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int size = getSize(*head);
 	unsigned int i = 0;
 	listint_t *firstNode;
+	listint_t *newNode;
 
-	if (*head == NULL)
+	newNode = malloc(sizeof(listint_t));
+	newNode->n = n;
+	newNode->next = NULL;
+	if (idx > size)
 		return (NULL);
-
-	firstNode = *head;
-	while (firstNode != NULL && (i != idx))
+	else if (idx == 0)
 	{
-		firstNode = firstNode->next;
-		i++;
+		newNode->next = *head;
+		*head = newNode;
+		return (*head);
 	}
-	if (i != idx)
-		return (NULL);
-	firstNode->n = n;
-	return (firstNode);
+	else
+	{
+		firstNode = *head;
+		while (firstNode != NULL && (i < idx))
+		{
+			firstNode = firstNode->next;
+			i++;
+		}
+
+		if (firstNode != NULL && (newNode != NULL))
+		{
+			newNode->next = firstNode->next;
+			firstNode->next = newNode;
+			return (firstNode);
+		}
+		else
+			return (NULL);
+	}
+}
+
+/**
+ * getSize - size of linkedlist
+ * @head: head node
+ * Return: size
+ */
+unsigned int getSize(listint_t *head)
+{
+	unsigned int size = 0;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		size++;
+	}
+	return (size);
 }
