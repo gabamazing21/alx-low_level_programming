1383961f0b9d27f8ae92bb7ccb06c3973cffe4bc
gabriel <gabeamazing21@gmail.com>
2023-09-27 14:44:25 +0000

    add empty string when it's null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,18 +10,17 @@ char *str_concat(char *s1, char *s2)
 {
 	int len, i, j;
 	char *s;
-
 	len = _strlen(s1) + _strlen(s2) + 2;
 	s = malloc(sizeof(char) * len);
 	if (s == NULL)
 		return (NULL);
-	if (s1 != NULL)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
 			s[i] = s1[i];
-	}
-	if (s2 != NULL)
-		for (j = 0; s2[j] != '\0'; j++, i++)
+	for (j = 0; s2[j] != '\0'; j++, i++)
 			s[i] = s2[j];
 	s[i] = '\0';
 	return (s);
