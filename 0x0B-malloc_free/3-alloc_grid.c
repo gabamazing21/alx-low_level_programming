f68ad00210207f4117c935a33496afb897f976d1
gabriel <gabeamazing21@gmail.com>
2023-09-27 22:00:55 +0000

    add point

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,13 +10,13 @@ int **alloc_grid(int width, int height)
 	int **d;
 	int i, j;
 
-	d = malloc(sizeof(int) * width);
+	d = malloc(sizeof(int *) * width);
 	if (d == NULL)
 		return (NULL);
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	for (i = 0; i < width; i++)
-		d[i] = malloc(sizeof(int) * height);
+		d[i] = malloc(sizeof(int *) * height);
 	for (i = 0; i < width; i++)
 		for (j = 0 ; j < height; j++)
 			d[i][j] = 0;
