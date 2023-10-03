e1b3f14628c2187a3113d7cb9853ec5a92d0a1ba
gabriel <gabeamazing21@gmail.com>
2023-10-03 02:55:31 +0000

    okay

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,21 +10,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, rd;
 	char *ch;
 	size_t n = 0;
+
 	if (filename == NULL)
 		return (0);
 	ch = malloc(sizeof(char) * letters);
 	if (ch == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(ch);
+		return (0);
+	}
 	rd = read(fd, ch, letters);
 	if (rd == -1)
+	{
+		free(ch);
+		close(fd);
 		return (0);
+	}
 
 	for (n = 0; ch[n] != '\0'; n++)
 	{
 		_putchar(ch[n]);
 
 	}
+	free(ch);
 	return (n);
 }
 
