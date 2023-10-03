d25b8beeaa9174b3454c2c809dc232529c9aaa30
gabriel <gabeamazing21@gmail.com>
2023-10-03 14:30:39 +0000

    add trunc flag and change mode to 777

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR, 0777);
+	if (text_content == NULL)
+	{
+		text_content = "\0";
+	}
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 777);
 	if (fd == -1)
 	{
 		return (-1);
