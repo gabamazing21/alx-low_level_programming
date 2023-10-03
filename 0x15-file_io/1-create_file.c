7ee6cb562c301e1ea3b620c4963348650a49675e
gabriel <gabeamazing21@gmail.com>
2023-10-03 14:09:32 +0000

    added how to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,45 @@
 #include "main.h"
+int _strlen(char *s);
+/**
+ * create_file - create file
+ * @filename: file name
+ * @text_content: text content
+ * Return: 1 or -1
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int n, fd;
-	fd = open(filename, O_CREAT | O_RDWR);
+	int fd, wr;
 
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR, 0777);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	wr = write(fd, text_content, _strlen(text_content));
+	if (wr == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
+/**
+ * _strlen - it return string lengh
+ * @s: strings to get it's length
+ * Return: it return lenght
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+
+	}
+	return (length);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,4 +6,5 @@ int _putchar(char c);
 #include <fcntl.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
