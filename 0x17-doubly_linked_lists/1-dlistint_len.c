d54a7553442295d9b03162c9bb8fc4532e27b241
Gabriel Daramola <gabamazing21@gmail.com>
2023-11-12 09:10:36 +0100

    returning number of element in double linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+/**
+ * dlistint_len - return number of element:
+ * @h: head node
+ * Return: return number of element
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t i = 0;
+
+	temp = h;
+	while (temp != 0)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
