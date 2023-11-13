ac4c2668c0438be769354d68c7979dca3f518fd5
gabriel <gabeamazing21@gmail.co>
2023-11-13 15:22:22 -0800

    adding the puts

--- 0x18-dynamic_libraries/6-puts2.c
+++ 0x18-dynamic_libraries/6-puts2.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string
+ * _puts - prints every other character of a string
  * @str: input string
  */
 
-void puts2(char *str)
+void _puts(char *str)
 {
 	int i;
 
