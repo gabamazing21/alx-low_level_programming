eedbb7850c83fd9561460ffa49626061e27b6ce4
gabriel <gabeamazing21@gmail.co>
2023-12-03 14:26:31 -0800

    betty cleard

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+
 	if (table == NULL)
 		return (NULL);
 	table->array = calloc(size, sizeof(hash_node_t *));
