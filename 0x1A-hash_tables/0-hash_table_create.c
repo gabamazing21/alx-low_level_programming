c8fed5fec3e8c557f936ee8d1543526426292783
gabriel <gabeamazing21@gmail.co>
2023-12-03 03:17:34 -0800

    all test is okay now

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,11 +7,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t* table = (hash_table_t *) malloc(sizeof(hash_table_t));
-	
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+
 	if (!table)
 		return (NULL);
-	table->array = calloc(size, sizeof(hash_node_t*));
+	table->array = calloc(size, sizeof(hash_node_t *));
 	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
