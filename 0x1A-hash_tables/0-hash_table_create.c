18d649e52cd1daa5c2e7fc5b5a406178e6c98c75
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-03 10:48:12 +0100

    check for memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,9 +6,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t* table = (hash_table_t *) malloc(sizeof(hash_table_t));
-	table->key = (char *) malloc(sizeof(char));
-	table->value = (char *) malloc(sizeof(char));
+	table->array = calloc(size, sizeof(hash_node_t*));
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
 	return table;
 }
 
