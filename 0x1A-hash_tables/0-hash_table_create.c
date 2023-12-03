61e33d8bbaa1f881ef01f41007579f2420dcfe2b
gabriel <gabeamazing21@gmail.co>
2023-12-03 07:22:30 -0800

    make changes to the merging

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
-<<<<<<< HEAD
-	if (!table)
-=======
 	if (table == NULL)
->>>>>>> 84730f60a9a0d06a198c6b76272ebe58bda1c8d1
 		return (NULL);
 	table->array = calloc(size, sizeof(hash_node_t *));
 	for (i = 0; i < size; i++)
