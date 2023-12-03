5772fa00e97d726cb4cf8b2c95a694e62ad0b53b
gabriel <gabeamazing21@gmail.co>
2023-12-03 14:24:49 -0800

    added size , check if array is null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
-
 	if (table == NULL)
 		return (NULL);
 	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
+	table->size = size;
 	return (table);
 }
 
