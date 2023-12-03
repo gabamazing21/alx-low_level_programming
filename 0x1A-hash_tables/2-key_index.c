c4defc2a06fd7155101b4bd683ff1f22e21cd713
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-04 00:10:58 +0100

    create index for the key's using hash

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+/**
+ * key_index - get the index for a key
+ * @size: size of hash_table
+ * @key: key for hash_table
+ * Return: index
+ */
+#include "hash_tables.h"
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	(void) size;
+	index = hash_djb2(key);
+	return (index);
+}
