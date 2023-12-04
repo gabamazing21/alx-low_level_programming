b990974c6e2b257b6a257ed7a7fc4ad55babd8d3
Gabriel Daramola <gabamazing21@gmail.com>
2023-12-04 16:07:27 +0100

    fixed all issues

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,24 +4,44 @@
  * @key: key
  * @value: value
  * Return: 1 if success 0 for failure
- */i
+ */
 #include "hash_tables.h"
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 
 	unsigned long int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *current_item = ht->array[index];
-
 	if (current_item == NULL)
 	{
+		hash_node_t* head = (hash_node_t *) malloc(sizeof(hash_node_t));
+		if (head == NULL)
+		{
+			return (0);
+		}
+		head->key = strdup(key);
+		if (head->key == NULL)
+		{
+			free(head->key);
+			free(head);
+			return (0);
+		}
+		head->value = strdup(value);
+		if (head->value == NULL)
+		{
+			free(head->key);
+			free(head->value);
+			free(head);
+			return (0);
+		}
+		head->next = NULL;
+
 		if (ht->size == index)
 		{
 			printf("table is full");
 			free_table(ht);
 			return (0);
 		}
-		strcpy(current_item->value, value);
-		current_item->next = NULL;
+		ht->array[index] = head;
 		return (1);
 	}
 	else
@@ -29,24 +49,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(current_item->key, key))
 		{
 			strcpy(current_item->value, value);
-			current_item->next = NULL;
 			return (1);
 		}
 		else
 		{
-			hash_node_t *head = malloc(sizeof(hash_node_t));
-
-			if (head == NULL)
-			{
-				free(head);
-				return (0);
-			}
-			strcpy(head->key, key);
-			strcpy(head->value, value);
-			head->next = NULL;
-			current_item->next = head;
+		hash_node_t* head = (hash_node_t *) malloc(sizeof(hash_node_t));
+		if (head == NULL)
+		{
+			return (0);
+		}
+		head->key = strdup(key);
+		if (head->key == NULL)
+		{
+			free(head->key);
 			free(head);
-			return (1);
+			return (0);
+		}
+		head->value = strdup(value);
+		if (head->value == NULL)
+		{
+			free(head->key);
+			free(head->value);
+			free(head);
+			return (0);
+		}
+		head->next = current_item;
+		ht->array[index]->next = head;
+		return (1);
 		}
 	}
 }
